60d0776380428a4278a2f7dbe6ef7a6ff05dbf4f
SAMTEC15 <samuel.orji@decagon.dev>
2023-11-27 01:16:46 +0100

    Auth Controller

--- WebAPI/Controllers/AuthorityController.cs
+++ WebAPI/Controllers/AuthorityController.cs
@@ -1,4 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using WebAPI.Authority;
 
 namespace WebAPI.Controllers
@@ -6,15 +10,22 @@ namespace WebAPI.Controllers
     [ApiController]
     public class AuthorityController : Controller
     {
+        private readonly IConfiguration _configuration;
+
+        public AuthorityController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
         [HttpPost("auth")]
         public IActionResult Authenticate([FromBody] AppCredential credential)
         {
             if(AppRespository.Authenticate(credential.ClientId, credential.Secret))
             {
+                var expiresAt = DateTime.UtcNow.AddMinutes(10);
                 return Ok(new
                 {
-                    access_token = CreateToken(credential.ClientId),
-                    expires_at = DateTime.Now.AddMinutes(10)
+                    access_token = CreateToken(credential.ClientId, expiresAt),
+                    expires_at = expiresAt
                 });
             }
 
@@ -26,9 +37,28 @@ namespace WebAPI.Controllers
            return new UnauthorizedObjectResult(problemDetails);
         }
 
-        private string CreateToken(string clientId)
+        private string CreateToken(string clientId, DateTime expiresAt)
         {
-            return string.Empty;
+            //Algo, Payload, Siging Key
+            var app = AppRespository.GetApplicationByClientId(clientId);
+            var claims = new List<Claim>
+            {
+                new Claim("AppName", app?.ApplicationName??string.Empty),
+                new Claim("Read", (app?.Scopes??string.Empty).Contains("read")?"true":"false"),
+                new Claim("Write", (app?.Scopes??string.Empty).Contains("write")?"true":"false")
+            };
+
+            var secretKey = Encoding.ASCII.GetBytes(_configuration.GetValue<string>("Key"));
+
+            var jwt = new JwtSecurityToken(
+                signingCredentials: new SigningCredentials(
+                   new SymmetricSecurityKey(secretKey),
+                SecurityAlgorithms.HmacSha256Signature),
+                claims: claims,
+                expires: expiresAt,
+                notBefore: DateTime.UtcNow
+                );
+            return new JwtSecurityTokenHandler().WriteToken(jwt);
         }
     }
 }
